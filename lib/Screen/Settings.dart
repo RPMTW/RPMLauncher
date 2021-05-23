ecba96709068f50ce7e1b2c24209f3d48b3f8547
菘菘 <rrt467778@gmail.com>
2021-05-23 10:42:18 +0800

    自動下載 Java config

--- lib/Screen/Settings.dart
+++ lib/Screen/Settings.dart
@@ -5,37 +5,42 @@ import '../main.dart';
 import 'package:path/path.dart';
 import 'package:xdg_directories/xdg_directories.dart';
 import 'dart:convert';
+
 var java_path;
 
 class SettingScreen_ extends State<SettingScreen> {
   late io.Directory ConfigFolder;
   late io.File ConfigFile;
   late Map config;
+  bool AutoJava = true;
+  var ConfigPath = ["java_path","auto_java"];
+
   @override
   void initState() {
     ConfigFolder = configHome;
-    ConfigFile = io.File(join(ConfigFolder.absolute.path, "RPMLauncher","config.json"));
+    ConfigFile =
+        io.File(join(ConfigFolder.absolute.path, "RPMLauncher", "config.json"));
     config = json.decode(ConfigFile.readAsStringSync());
-    if (config.containsKey("java_path")) {
+    if (config.containsKey(ConfigPath)) {
       controller_java.text = config["java_path"];
+      AutoJava = config["auto_java"];
     }
     super.initState();
-    controller_java.addListener(() async{
-      bool exists_=await io.File(controller_java.text).exists();
-      if (controller_java.text.split("/").reversed.first == "java" &&exists_ ||
-          controller_java.text.split("/").reversed.first == "javaw"&&exists_) {
+    controller_java.addListener(() async {
+      bool exists_ = await io.File(controller_java.text).exists();
+      if (controller_java.text.split("/").reversed.first == "java" && exists_ ||
+          controller_java.text.split("/").reversed.first == "javaw" &&
+              exists_) {
         valid_java_bin = Colors.blue;
-        config["java_path"]=controller_java.text;
+        config["java_path"] = controller_java.text;
       } else {
         valid_java_bin = Colors.red;
       }
-      setState(() {
-
-      });
+      setState(() {});
     });
   }
-  void openSelect(BuildContext context) async {
 
+  void openSelect(BuildContext context) async {
     final file = await FileSelectorPlatform.instance.openFile();
     if (file == null) {
       return;
@@ -44,18 +49,24 @@ class SettingScreen_ extends State<SettingScreen> {
       java_path = file.path;
       controller_java.text = java_path;
       java_path = controller_java.text;
-      config["java_path"]=java_path;
-    }else{
-      showDialog(context: context, builder: (context){
-        return AlertDialog(title: const Text("尚未偵測到 Java"),content: Text("這個檔案不是 java 或 javaw。"),actions: <Widget>[
-          TextButton(
-            child: const Text('確認'),
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-          ),
-        ],);
-      });
+      config["java_path"] = java_path;
+    } else {
+      showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: const Text("尚未偵測到 Java"),
+              content: Text("這個檔案不是 java 或 javaw。"),
+              actions: <Widget>[
+                TextButton(
+                  child: const Text('確認'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            );
+          });
     }
   }
 
@@ -64,11 +75,9 @@ class SettingScreen_ extends State<SettingScreen> {
     fontSize: 20.0,
   );
   var controller_java = TextEditingController();
-  Color valid_java_bin=Colors.white;
-
+  Color valid_java_bin = Colors.white;
 
   Widget build(BuildContext context) {
-
     return new Scaffold(
       appBar: new AppBar(
         title: new Text("設定選單"),
@@ -104,24 +113,34 @@ class SettingScreen_ extends State<SettingScreen> {
                     child: TextField(
                   controller: controller_java,
                   decoration: InputDecoration(
-                      hintText: "Java 路徑",
+                    hintText: "Java 路徑",
                     enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                          color: valid_java_bin, width: 5.0),
+                      borderSide: BorderSide(color: valid_java_bin, width: 5.0),
                     ),
                     focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                          color: valid_java_bin, width: 3.0),
-                    ),),
+                      borderSide: BorderSide(color: valid_java_bin, width: 3.0),
+                    ),
+                  ),
                 )),
                 TextButton(
                     onPressed: () {
                       openSelect(context);
-
                     },
                     child: Text("選擇 Java")),
               ])),
-              ListTile()
+              ListTile(),
+              ListTile(
+                  title: Row(children: [
+                    Text("是否啟用自動下載 Java"),
+                    Switch(
+                        value: AutoJava,
+                        onChanged: (value) {
+                          setState(() {
+                            AutoJava = !AutoJava;
+                            config["auto_java"] = !AutoJava;
+                          });
+                        })
+                  ]))
             ],
           )),
     );
