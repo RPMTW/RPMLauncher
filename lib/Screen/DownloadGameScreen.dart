7b324aca7ca7930248db01821a7cad7d8826f2e6
菘菘 <rrt467778@gmail.com>
2021-07-31 13:08:31 +0800

    fix some bug

--- lib/Screen/DownloadGameScreen.dart
+++ lib/Screen/DownloadGameScreen.dart
@@ -11,6 +11,8 @@ class DownloadGameScreen_ extends State<DownloadGameScreen> {
   late var Data;
   late var ModLoaderName;
   late var IsFabric;
+  late var IsCompatibleVersion;
+  late var finish;
 
   DownloadGameScreen_(
       border_colour_, name_controller_, InstanceDir_, Data_, ModLoaderName_) {
@@ -29,41 +31,47 @@ class DownloadGameScreen_ extends State<DownloadGameScreen> {
         ModLoader().Fabric;
   }
 
-  FabricIncompatibleErr(value, context) {
-    if (value) return;
-    showDialog(
-      barrierDismissible: false,
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          contentPadding: const EdgeInsets.all(16.0),
-          title: Text("錯誤資訊"),
-          content: Text("目前選擇的Minecraft版本與選擇的模組載入器版本不相容"),
-          actions: <Widget>[
-            TextButton(
-              child: Text("ok"),
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    new MaterialPageRoute(
-                        builder: (context) => VersionSelection()));
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
-
   Widget build(BuildContext context) {
     if (IsFabric) {
-      FabricAPI()
-          .IsCompatibleVersion(Data["id"])
-          .then((value) => FabricIncompatibleErr(value, context));
-    }else{
-      return AddInstanceWidget(border_colour, InstanceDir, name_controller, Data);
+      FabricAPI().IsCompatibleVersion(Data["id"]).then((value) => setState(() {
+            IsCompatibleVersion = value;
+            finish = true;
+            return;
+          }));
+      try {
+        if (IsCompatibleVersion) {
+          return AddInstanceWidget(
+              border_colour, InstanceDir, name_controller, Data);
+        } else {
+          return AlertDialog(
+            contentPadding: const EdgeInsets.all(16.0),
+            title: Text("錯誤資訊"),
+            content: Text("目前選擇的Minecraft版本與選擇的模組載入器版本不相容"),
+            actions: <Widget>[
+              TextButton(
+                child: Text("ok"),
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      new MaterialPageRoute(
+                          builder: (context) => VersionSelection()));
+                },
+              ),
+            ],
+          );
+        }
+      } catch (err) {}
+    } else {
+      return AddInstanceWidget(
+          border_colour, InstanceDir, name_controller, Data);
     }
-    return AddInstanceWidget(border_colour, InstanceDir, name_controller, Data);
+    return AlertDialog(
+      title: Column(
+        children:[
+          Text("正在檢查遊戲版本是否符合模組載入器所需版本\n",textAlign: TextAlign.center),
+          CircularProgressIndicator()
+        ]),
+    );
   }
 }
 
