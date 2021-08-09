35c148c69173fc07a70504263d6ec54ab90e4d1d
KyleLin921021 <43753315+KyleLin921021@users.noreply.github.com>
2021-08-09 09:02:52 +0800

    add murmurhash2

--- lib/Utility/utility.dart
+++ lib/Utility/utility.dart
@@ -16,7 +16,8 @@ class utility {
     if (Platform.isLinux) {
       await Process.run("xdg-open", [Dir.path]);
     } else if (Platform.isWindows) {
-      await Process.run("start", ["file:///${Dir.path.replaceAll(" ","%20")}"], runInShell: true);
+      await Process.run("start", ["file:///${Dir.path.replaceAll(" ", "%20")}"],
+          runInShell: true);
     } else if (Platform.isMacOS) {
       await Process.run("open", [Dir.path]);
     }
@@ -71,11 +72,14 @@ class utility {
     Map Result = {};
     String PackageName = lib["name"].toString().split(":")[0];
     String split_1 = lib["name"].toString().split("${PackageName}:").join("");
-    String FileVersion = split_1.split(":")[split_1.split(":").length - 1];
+    String FileVersion = split_1.split(":")[split_1
+        .split(":")
+        .length - 1];
     String Filename = split_1.replaceAll(":", "-");
     String split_2 = Filename.split(FileVersion)[0];
     String Url =
-        "${lib["url"]}${PackageName.replaceAll(".", "/")}/${split_2.substring(0, split_2.length - 1)}/${FileVersion}/${Filename}";
+        "${lib["url"]}${PackageName.replaceAll(".", "/")}/${split_2.substring(
+        0, split_2.length - 1)}/${FileVersion}/${Filename}";
 
     Result["Filename"] = "${Filename}.jar";
     Result["Url"] = "${Url}.jar";
@@ -100,8 +104,8 @@ class utility {
     return src.replaceAll("/", Platform.pathSeparator);
   }
 
-  static Future<bool> OpenJavaSelectScreen(
-      BuildContext context, JavaVersion) async {
+  static Future<bool> OpenJavaSelectScreen(BuildContext context,
+      JavaVersion) async {
     final file = await FileSelectorPlatform.instance.openFile();
     if (file == null) {
       return false;
@@ -131,7 +135,56 @@ class utility {
     }
   }
 
-  static String DuplicateNameHandler(String Name){
+  static String DuplicateNameHandler(String Name) {
     return Name + "(${i18n.Format("gui.copy")})";;
+  }
+
+  static int murmurhash2(String str, int seed) {
+    var l = str.length,
+        h = seed ^ l,
+        i = 0,
+        k;
+
+    while (l >= 4) {
+      k =
+      ((str.codeUnitAt(i) & 0xff)) |
+      ((str.codeUnitAt(++i) & 0xff) << 8) |
+      ((str.codeUnitAt(++i) & 0xff) << 16) |
+      ((str.codeUnitAt(++i) & 0xff) << 24);
+
+      k = (((k & 0xffff) * 0x5bd1e995) +
+          ((((k >> 16) * 0x5bd1e995) & 0xffff) << 16));
+      k ^= k >> 24;
+      k = (((k & 0xffff) * 0x5bd1e995) +
+          ((((k >> 16) * 0x5bd1e995) & 0xffff) << 16));
+
+      h = (((h & 0xffff) * 0x5bd1e995) +
+          ((((h >> 16) * 0x5bd1e995) & 0xffff) << 16)) ^ k;
+
+      l -= 4;
+      ++i;
+    }
+
+    switch (l) {
+      case 3:
+        h ^= (str.codeUnitAt(i + 2) & 0xff) << 16;
+        continue len2;
+      len2:
+      case 2:
+        h ^= (str.codeUnitAt(i + 1) & 0xff) << 8;
+        continue len1;
+      len1:
+      case 1:
+        h ^= (str.codeUnitAt(i) & 0xff);
+        h = (((h & 0xffff) * 0x5bd1e995) +
+            ((((h >> 16) * 0x5bd1e995) & 0xffff) << 16));
+    }
+
+    h ^= h >> 13;
+    h = (((h & 0xffff) * 0x5bd1e995) +
+        ((((h >> 16) * 0x5bd1e995) & 0xffff) << 16));
+    h ^= h >> 15;
+
+    return h >> 0;
   }
 }
