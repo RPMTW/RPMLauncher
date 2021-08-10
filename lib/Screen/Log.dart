e7bfe7c4c864a26c71c8266e8509ce750747a880
菘菘 <rrt467778@gmail.com>
2021-08-10 20:05:49 +0800

    fix a bug

--- lib/Screen/Log.dart
+++ lib/Screen/Log.dart
@@ -187,6 +187,7 @@ class LogScreen_ extends State<LogScreen> {
       this.onData.forEach((event) {
         if (ShowLog) {
           log_ += data;
+          setState(() {});
         }
       });
     });
