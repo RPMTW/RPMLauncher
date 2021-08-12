dcfc6ff15b42aa292bfd64f84cb75f0a9b9ac86b
菘菘 <rrt467778@gmail.com>
2021-08-12 20:29:01 +0800

    一些例外處理

--- lib/Mod/CurseForge/ModPackHandler.dart
+++ lib/Mod/CurseForge/ModPackHandler.dart
@@ -7,42 +7,50 @@ import 'package:flutter/material.dart';
 
 class CurseModPackHandler {
   static Widget Setup(File ModPackZipFile, [String ModPackIconUrl = ""]) {
-    return FutureBuilder(
-        future: ModPackZipFile.readAsBytes(),
-        builder: (context, AsyncSnapshot snapshot) {
-          if (snapshot.hasData) {
-            final archive = ZipDecoder().decodeBytes(snapshot.data);
-            bool isModPack =
-                archive.files.any((file) => file.name == "manifest.json");
-            if (isModPack) {
-              return WillPopScope(
-                onWillPop: () => Future.value(false),
-                child: DownloadCurseModPack(archive, ModPackIconUrl),
-              );
+    try {
+      return FutureBuilder(
+          future: ModPackZipFile.readAsBytes(),
+          builder: (context, AsyncSnapshot snapshot) {
+            if (snapshot.hasData) {
+              try {
+                final Archive archive = ZipDecoder().decodeBytes(snapshot.data);
+                bool isModPack =
+                    archive.files.any((file) => file.name == "manifest.json");
+                if (isModPack) {
+                  return WillPopScope(
+                    onWillPop: () => Future.value(false),
+                    child: DownloadCurseModPack(archive, ModPackIconUrl),
+                  );
+                } else {
+                  return AlertDialog(
+                      contentPadding: const EdgeInsets.all(16.0),
+                      title: Text(i18n.Format("gui.error.info")),
+                      content: Text("錯誤的模組包格式"),
+                      actions: <Widget>[
+                        TextButton(
+                          child: Text(i18n.Format("gui.ok")),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        )
+                      ]);
+                }
+              } on ArchiveException {
+                return CircularProgressIndicator();
+              }
             } else {
               return AlertDialog(
-                  contentPadding: const EdgeInsets.all(16.0),
-                  title: Text(i18n.Format("gui.error.info")),
-                  content: Text("錯誤的模組包格式"),
-                  actions: <Widget>[
-                    TextButton(
-                      child: Text(i18n.Format("gui.ok")),
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                    )
-                  ]);
+                title: Text("正在解析模組包資訊中..."),
+                content: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [CircularProgressIndicator()],
+                ),
+              );
             }
-          } else {
-            return AlertDialog(
-              title: Text("正在解析模組包資訊中..."),
-              content: Row(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [CircularProgressIndicator()],
-              ),
-            );
-          }
-        });
+          });
+    } on FormatException {
+      return CircularProgressIndicator();
+    }
   }
 }
