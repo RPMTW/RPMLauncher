fdafe3725f54b7039b4fb94b650d11e62a100f84
菘菘 <rrt467778@gmail.com>
2021-09-24 23:04:21 +0800

    新增：更多更多的單元測試

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -8,8 +8,12 @@ import 'package:no_context_navigation/no_context_navigation.dart';
 import 'package:rpmlauncher/LauncherInfo.dart';
 import 'package:rpmlauncher/Screen/About.dart';
 import 'package:rpmlauncher/Screen/Account.dart';
+import 'package:rpmlauncher/Screen/CurseForgeMod.dart';
+import 'package:rpmlauncher/Screen/CurseForgeModPack.dart';
+import 'package:rpmlauncher/Screen/FTBModPack.dart';
 import 'package:rpmlauncher/Screen/Settings.dart';
 import 'package:rpmlauncher/Screen/VersionSelection.dart';
+import 'package:rpmlauncher/Utility/Loggger.dart';
 import 'package:rpmlauncher/Utility/Updater.dart';
 import 'package:rpmlauncher/Utility/i18n.dart';
 import 'package:rpmlauncher/path.dart';
@@ -44,24 +48,23 @@ void main() async {
       expect(
           (await Updater.checkForUpdate(VersionTypes.debug)).needUpdate, false);
     });
+    test('Logger test', () => {Logger.currentLogger.send('Hello World')});
     testWidgets('Settings Screen', (WidgetTester tester) async {
       await TestUttily.BaseTestWidget(tester, SettingScreen());
+      expect(find.text(i18n.format("settings.title")), findsOneWidget);
     });
     testWidgets('About Screen', (WidgetTester tester) async {
       await TestUttily.BaseTestWidget(tester, AboutScreen());
     });
     testWidgets('Account Screen', (WidgetTester tester) async {
-      await tester.runAsync(() async {
-        await TestUttily.BaseTestWidget(tester, AccountScreen());
-      });
-    });
-    testWidgets('Settings Screen', (WidgetTester tester) async {
-      await TestUttily.BaseTestWidget(tester, SettingScreen());
+      await TestUttily.BaseTestWidget(tester, AccountScreen(), async: true);
     });
     testWidgets('VersionSelection Screen', (WidgetTester tester) async {
-      await tester.runAsync(() async {
-        await TestUttily.BaseTestWidget(tester, VersionSelection());
-      });
+      await TestUttily.BaseTestWidget(tester, VersionSelection(), async: true);
+    });
+    testWidgets('ModPackage Screen', (WidgetTester tester) async {
+      await TestUttily.BaseTestWidget(tester, CurseForgeModPack(), async: true);
+      await TestUttily.BaseTestWidget(tester, FTBModPack(), async: true);
     });
   });
 }
@@ -73,7 +76,7 @@ class TestUttily {
   }
 
   static Future<void> BaseTestWidget(WidgetTester tester, Widget child,
-      [bool async = false]) async {
+      {bool async = false}) async {
     if (async) {
       await tester.runAsync(() async {
         await _Pump(tester, child);
