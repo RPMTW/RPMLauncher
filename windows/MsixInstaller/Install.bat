e7f1621779003d62e87fe6fa4fb1bfcad43bb8ff
SiongSng <rrt467778@gmail.com>
2021-10-31 09:26:48 +0800

    錯誤修復：Windows 安裝程式

--- windows/MsixInstaller/Install.bat
+++ windows/MsixInstaller/Install.bat
@@ -4,9 +4,13 @@ CD /D "%~dp0"
 
 :: 安裝憑證
 
+echo "安裝憑證中.."
+
 Powershell.exe Import-PfxCertificate -Password (ConvertTo-SecureString -AsPlainText -Force "rpmtw") -FilePath ./CERTIFICATE.pfx -CertStoreLocation Cert:\LocalMachine\Root
 
 :: 執行MSIX安裝程式
+
+echo "執行安裝程式中..."
 
 ./rpmlauncher.msix
 
