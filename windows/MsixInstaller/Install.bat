850055f135e86a97d2918f8658c1b9aafeb73138
菘菘 <rrt467778@gmail.com>
2021-10-31 10:01:55 +0800

    修復安裝程式

--- windows/MsixInstaller/Install.bat
+++ windows/MsixInstaller/Install.bat
@@ -1,5 +1,7 @@
 pushd "%CD%"
 
+if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
+
 CD /D "%~dp0"
 
 :: 安裝憑證
@@ -12,6 +14,4 @@ Powershell.exe Import-PfxCertificate -Password (ConvertTo-SecureString -AsPlainT
 
 echo "執行安裝程式中..."
 
-./rpmlauncher.msix
-
-exit 0
+Powershell.exe ./rpmlauncher.msix
