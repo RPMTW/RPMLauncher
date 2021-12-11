b20d932247730fb26937850ac0440703befbf0ea
SiongSng <rrt467778@gmail.com>
2021-12-11 10:24:15 +0800

    Enhancement: Text input box for global settings page
    增強：改善設定頁面文字輸入框

--- lib/Widget/RPMTW-Design/RPMTextField.dart
+++ lib/Widget/RPMTW-Design/RPMTextField.dart
@@ -1,28 +1,61 @@
 import 'package:flutter/material.dart';
 
-class RPMTextField extends TextField {
-  RPMTextField({
-    Key? key,
-    TextEditingController? controller,
-    ValueChanged<String>? onChanged,
-    String? hintText,
-    TextAlign textAlign = TextAlign.center,
-    TextInputType? keyboardType,
-    VoidCallback? onEditingComplete,
-  }) : super(
-            key: key,
-            controller: controller,
-            decoration: InputDecoration(
-              hintText: hintText,
-              enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.white12, width: 3.0),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.lightBlue, width: 3.0),
-              ),
-            ),
-            textAlign: textAlign,
-            keyboardType: keyboardType,
-            onEditingComplete: onEditingComplete,
-            onChanged: onChanged);
+class RPMTextField extends StatefulWidget {
+  final TextEditingController? controller;
+  final ValueChanged<String>? onChanged;
+  final bool Function(String value)? verify;
+  final String? hintText;
+  final TextAlign textAlign;
+  final TextInputType? keyboardType;
+  final VoidCallback? onEditingComplete;
+
+  const RPMTextField({
+    this.controller,
+    this.onChanged,
+    this.verify,
+    this.hintText,
+    this.textAlign = TextAlign.center,
+    this.keyboardType,
+    this.onEditingComplete,
+  });
+
+  @override
+  State<RPMTextField> createState() => _RPMTextFieldState();
+}
+
+class _RPMTextFieldState extends State<RPMTextField> {
+  Color enabledColor = Colors.white12;
+
+  Color focusedColor = Colors.lightBlue;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+        controller: widget.controller,
+        decoration: InputDecoration(
+          hintText: widget.hintText,
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: enabledColor, width: 3.0),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: focusedColor, width: 3.0),
+          ),
+        ),
+        textAlign: widget.textAlign,
+        keyboardType: widget.keyboardType,
+        onEditingComplete: widget.onEditingComplete,
+        onChanged: (value) {
+          bool verifyResult = widget.verify?.call(value) ?? true;
+
+          if (!verifyResult) {
+            enabledColor = Colors.red;
+            focusedColor = Colors.red;
+          } else {
+            enabledColor = Colors.white12;
+            focusedColor = Colors.lightBlue;
+            widget.onChanged?.call(value);
+          }
+          setState(() {});
+        });
+  }
 }
